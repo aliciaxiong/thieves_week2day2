f4aa327242b5513a955bf65b39c70ff9d78b52c5
Your Name aliciaxiong <aliciaxiong22@gmail.com>
2023-10-17 22:48:44 -0700

    finished question 1

---
+++ scratchpaper.py
@@ -0,0 +1,32 @@
+
+l_1 = [1,11,14,5,8,9]
+def num_list(l_1):
+      for num in l_1:
+        if num < 10:
+            return num_list(l_1)
+
+l_1 = [1,2,3,4,5,6]
+l_2 = [3,4,5,6,7,8,10]
+
+def sortcode(main_list): 
+    main_list = l_1 + l_2
+    main_list.sort()
+    return sortcode(main_list)
+
+l_1 = [1,2,3,4,5,6]
+l_2 = [3,4,5,6,7,8,10]
+
+def sortcode(main_list): 
+    if main_list in num == l_1 + l_2:
+        num = main_list.sort()
+        return main_list
+    
+    def sortcode(main_list): 
+        main_list = (l_1 + l_2)
+    return (main_list.sort)
+    sortcode(main_list)
+
+    def sortcode(main_list): 
+        main_list = (l_1 + l_2)
+        main_list.sort()
+        return main_list
